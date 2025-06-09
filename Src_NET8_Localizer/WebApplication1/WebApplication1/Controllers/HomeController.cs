6a71469e640d1cdc2dcdf0d2721579a90e667dea
testUser1 <testUser1@a.com>
2025-06-09 17:50:10 +0900

    1

--- Src_NET8_Localizer/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ Src_NET8_Localizer/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,32 +1,36 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly IStringLocalizer<HomeController> _localizer;
+
+    public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
+      _localizer = localizer;
     }
+
+    public IActionResult Index()
+    {
+      ViewBag.Message = _localizer["ControllerString1"];
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
--- Src_NET8_Localizer/WebApplication1/WebApplication1/Program.cs
+++ Src_NET8_Localizer/WebApplication1/WebApplication1/Program.cs
@@ -1,14 +1,46 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+  var supportedCultures = new[] { "ja", "en" };
+  options.SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+});
+
+builder.Services.AddMvc()
+  .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+  .AddDataAnnotationsLocalization();
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+  .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+  .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+  var supportedCultures = new[]
+  {
+    new System.Globalization.CultureInfo("en"),
+    new System.Globalization.CultureInfo("ja")
+  };
+
+  options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("ja");
+  options.SupportedCultures = supportedCultures;
+  options.SupportedUICultures = supportedCultures;
+});
 
 var app = builder.Build();
+app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+  app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
@@ -17,7 +49,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+  name: "default",
+  pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
